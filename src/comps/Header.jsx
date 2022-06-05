6c20c69ac0f50798cbccd6c413d1ac8206e36299
Sherly1001 <Sherly1001@users.noreply.github.com>
2022-06-05 23:53:41 +0700

    chore: format code

--- src/comps/Header.jsx
+++ src/comps/Header.jsx
@@ -5,9 +5,13 @@ import './Header.scss';
 const Header = ({ className }) => {
   window['sher'] = useStore();
 
-  return <div className={'header ' + className}>
-    <div class =" topic"><b class="font">ユーアールエルを省略するウェブサイト</b></div>
-  </div>;
+  return (
+    <div className={'header ' + className}>
+      <div className=" topic">
+        <b className="font">ユーアールエルを省略するウェブサイト</b>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
