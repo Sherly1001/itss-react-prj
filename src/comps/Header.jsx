e78a1744a99dfc01ec37cab2a7701a3e5ba68a00
Đinh Quốc Cường <quoccuong137@gmail.com>
2022-06-05 23:39:34 +0700

    Cuong Header

--- src/comps/Header.jsx
+++ src/comps/Header.jsx
@@ -1,9 +1,13 @@
 import { useStore } from '../store';
 
+import './Header.scss';
+
 const Header = ({ className }) => {
   window['sher'] = useStore();
 
-  return <div className={'header ' + className}>Header</div>;
+  return <div className={'header ' + className}>
+    <div class =" topic"><b class="font">ユーアールエルを省略するウェブサイト</b></div>
+  </div>;
 };
 
 export default Header;
