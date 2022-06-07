e5cd6303dc115bb010bd9738c01644edc884b54b
ninhtdat <ninhtdat@gmail.com>
2022-06-07 19:32:25 +0700

    update ads

--- src/comps/Ads.jsx
+++ src/comps/Ads.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 
+const imgUrls = [
+  'https://images7.alphacoders.com/110/thumbbig-1105631.webp',
+  'https://images5.alphacoders.com/109/thumbbig-1099191.webp',
+  'https://images8.alphacoders.com/112/thumbbig-1129503.webp',
+];
 const Ads = ({ className }) => {
-  const imgUrls = [
-    'https://images7.alphacoders.com/110/thumbbig-1105631.webp',
-    'https://images5.alphacoders.com/109/thumbbig-1099191.webp',
-    'https://images8.alphacoders.com/112/thumbbig-1129503.webp',
-  ]
   const [url, setUrl] = useState(0);
 
   useEffect(() => {
