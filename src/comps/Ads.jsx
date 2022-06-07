fe79978babc9e030c52e87b3502f48181339b6ad
ninhtdat <ninhtdat@gmail.com>
2022-06-07 12:58:57 +0700

    add ads

--- src/comps/Ads.jsx
+++ src/comps/Ads.jsx
@@ -1,5 +1,8 @@
 const Ads = ({ className }) => {
-  return <div className={'ads ' + className}>Ads</div>;
+  return <div className={'ads ' + className}>
+    <img src="https://images7.alphacoders.com/110/thumbbig-1105631.webp" id="image" width={420} height={105.2}></img>
+  </div>;
+  
 };
 
 export default Ads;
