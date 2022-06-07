d954732d3e8d41d7302a8eaec4525fa8087709d8
Sherly1001 <Sherly1001@users.noreply.github.com>
2022-06-07 23:53:01 +0700

    feat: add new url

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ const App = () => {
     <div className="app container grid gap-2 m-auto">
       <Header className="bg-red-200" />
       <div className="main gap-2 grid md:grid-cols-3 md:grid-rows-2">
-        <NewUrlForm className="bg-green-200" />
-        <Ads className="bg-yellow-200 md:row-start-2" />
+        <NewUrlForm className="" />
+        <Ads className="md:row-start-2" />
         <ListUrls className="bg-blue-200 md:col-span-2 md:row-span-2" />
       </div>
-      <Footer className='bg-red-200 footer'/>
+      <Footer className="bg-red-200 footer" />
     </div>
   );
 };
--- src/comps/NewUrlForm.jsx
+++ src/comps/NewUrlForm.jsx
@@ -1,5 +1,74 @@
+import { useState } from 'react';
+import { useStore } from '../store';
+
 const NewUrlForm = ({ className }) => {
-  return <div className={'new-url-form ' + className}>New Url Form</div>;
+  const [url, setUrl] = useState('');
+  const [code, setCode] = useState('');
+  const [err, setErr] = useState(null);
+
+  const { addUrl } = useStore();
+
+  const onSubmit = async () => {
+    try {
+      setErr(null);
+      await addUrl(url, code);
+    } catch (e) {
+      setErr(e.toString());
+    }
+  };
+
+  return (
+    <div className={'new-url-form w-full ' + className}>
+      <form
+        className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={onSubmit}
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="URL"
+          >
+            Long URL to short
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="URL"
+            type="text"
+            placeholder="Enter URL"
+            required
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="code"
+          >
+            Code
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="code"
+            type="text"
+            placeholder="Enter code"
+            required
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+          />
+          {err ? <p className="text-red-500 text-xs italic">{err}</p> : ''}
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Shorten URL
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default NewUrlForm;
